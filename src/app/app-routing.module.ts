e52d5651816e800cd2bc5ba4f9c3f8428b44c52b
Fernanda Ferraz <fernanda_ferraz2015@outlook.com>
2021-03-04 16:23:22 -0300

    delete e put postagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { InicioComponent } from './inicio/inicio.component';
 import { TemasComponent } from './temas/temas.component';
 import { UserEditComponent } from './edit/user-edit/user-edit.component';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
+import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 
 
 const routes: Routes = [
@@ -16,7 +18,10 @@ const routes: Routes = [
   {path: 'inicio', component: InicioComponent},
   {path:'tema', component: TemasComponent},
   {path: 'user-edit/:id', component: UserEditComponent},
-  {path: 'quem-somos', component: QuemSomosComponent}
+  {path: 'quem-somos', component: QuemSomosComponent},
+  {path: 'postagem-edit/:id', component:PostagemEditComponent},
+  {path: 'postagem-delete/:id', component:PostagemDeleteComponent},
+  
 ];
 
 @NgModule({
